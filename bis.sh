c128782e5b9f2916bbbea25e975c7c0b6223ee8c
satelitlima00 <128956610+satelitlima00@users.noreply.github.com>
2025-12-03 21:03:17 +0700

    Update bis.sh

--- bis.sh
+++ bis.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-if ! pgrep -f bas.sh > /dev/null; then chmod +x ./bas.sh && ./bas.sh; else echo KOPI NE SIH PANAS...; fi
+if ! pgrep -f bas.sh > /dev/null; then chmod +x ./bas.sh && ./bas.sh; else echo KOPI NE SIH PANAS.........................; fi
